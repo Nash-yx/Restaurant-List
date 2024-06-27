b61544fbc7f3f9e7aea69af3c03560942cc60539
Nash-yx <nash.git1217@gmail.com>
2024-06-27 15:13:25 +0800

    build: add static files & json for model

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const portNum = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.redirect('/restaurant');
+});
+
+app.get('/restaurant', (req, res) => {
+  res.send('listing menu');
+});
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`read restaurant: ${id}`);
 });
 
 app.listen(portNum, () => {
