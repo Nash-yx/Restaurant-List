02aa5650571fd2f5310de56c9ee113749c825dec
Nash <nash.git1217@gmail.com>
2025-04-03 16:21:51 +0800

    add restaurant seeder

---
+++ seeders/20250403081346-initial-data.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'Restaurants',
+      Array.from({ length: 10 }).map((_, i) => {
+        return {
+          name: `restaurant-${i}`,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      })
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Todos', null);
+  },
+};
